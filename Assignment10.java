a334ac74d2aea170c32a078e53a64d3c9d19a20e
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2023-09-13 13:45:12 +0600

    Update Assignment10.java

--- Assignment10.java
+++ Assignment10.java
@@ -1,6 +1,14 @@
 // find sum of 1-10 using for loop
 public class Assignment10 {
   public static void main(String[] args) {
+     int sum=0;
+    for (int i=1; i<=10; i++)
+    {
+        sum+=i;
+    }
+        System.out.println("sum "+sum );
+        
+    }
     
   }
-}
+
